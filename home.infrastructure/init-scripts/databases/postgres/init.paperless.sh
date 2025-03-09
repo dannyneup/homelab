a0439a9d25a6aba3c9bcfa0e56ec34e786b046a7
Danny Neupauer <dannynp03@gmail.com>
2025-03-09 03:02:07 +0100

    cleanup

--- home.infrastructure/init-scripts/databases/postgres/init.paperless.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-psql <<EOF
-    create database $PAPERLESS_POSTGRES_DATABASE;
-    create user $PAPERLESS_POSTGRES_USER with encrypted password '$PAPERLESS_POSTGRES_PASSWORD';
-    revoke all privileges on database $PAPERLESS_POSTGRES_DATABASE from public;
-    grant all privileges on database $PAPERLESS_POSTGRES_DATABASE to $PAPERLESS_POSTGRES_USER;
-EOF
