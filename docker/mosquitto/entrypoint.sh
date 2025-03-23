e412d307f3cb029eaacb4866a386e1c5cc10956c
Danny Neupauer <dannynp03@gmail.com>
2025-03-23 15:46:45 +0100

    Revert "updated mosquitto entrypoint to fix permission error"
    This reverts commit 8faeb4cb6f04c1c9fd70634205758ada3ec2f32e.

--- docker/mosquitto/entrypoint.sh
+++ docker/mosquitto/entrypoint.sh
@@ -5,5 +5,5 @@ touch /mosquitto/config/passwd
 chmod 0700 /mosquitto/config/passwd
 mosquitto_passwd -b /mosquitto/config/passwd $USERNAME $PASSWORD
 
-chown mosquitto:mosquitto -R /mosquitto
+chown mosquitto:mosquitto /mosquitto/config/passwd
 /usr/sbin/mosquitto -c /mosquitto/config/mosquitto.conf
