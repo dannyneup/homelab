8faeb4cb6f04c1c9fd70634205758ada3ec2f32e
Danny Neupauer <dannynp03@gmail.com>
2025-03-23 15:32:28 +0100

    updated mosquitto entrypoint to fix permission error

--- docker/mosquitto/entrypoint.sh
+++ docker/mosquitto/entrypoint.sh
@@ -5,5 +5,5 @@ touch /mosquitto/config/passwd
 chmod 0700 /mosquitto/config/passwd
 mosquitto_passwd -b /mosquitto/config/passwd $USERNAME $PASSWORD
 
-chown mosquitto:mosquitto /mosquitto/config/passwd
+chown mosquitto:mosquitto -R /mosquitto
 /usr/sbin/mosquitto -c /mosquitto/config/mosquitto.conf
